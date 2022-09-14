3970182cd4e73cd780fa3d4710aca30f140bfba8
daniel <senyonjodaniel6@gamil.com>
2022-09-14 16:49:54 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pryce concepts-events and parties</title>
+    <link rel="stylesheet" href="css/home.css">
+    <?php.require."css/home.css"?>
+</head>
+<body>
+    <div class="top">
+        <div class="icon">
+            <span><?php include"icons/bars.svg"?></span>
+        </div>
+        <div class="menu">
+            Menu
+            <div class="dropdown">
+                <nav>
+                    <a href="#">
+                    <div class="icon1">
+                             <span><?php include"icons/house.svg"?></span>
+                        </div>
+                        Home
+                     </a>
+                     <div class="about">
+                        About Us
+                        <div class="icon1">
+                             <span><?php include"icons/angle-right.svg"?></span>
+                        </div>
+                        <div class="drop2">
+                           <nav>
+                            <a href="#">
+                               <div class="icon1">
+                                 <span><?php include"icons/neuter.svg"?></span>
+                                </div>
+                                company policy
+                            </a>
+                            <a href="#">
+                               <div class="icon1">
+                                 <span><?php include"icons/circle-user.svg"?></span>
+                                 </div>
+                                company profile
+                            </a>
+                            <a href="#">
+                               <div class="icon1">
+                                 <span><?php include"icons/network-wired.svg"?></span>
+                                 </div>
+                                Origin and founder
+
+                            </a>
+                           </nav>
+                        </div>
+                     </div>
+                    <a href="#">
+                        <div class="icon1">
+                             <span><?php include"icons/explosion.svg"?></span>
+                        </div>
+                        Events
+                    </a>
+                    <a href="#">
+                    <div class="icon1">
+                             <span><?php include"icons/calendar-check.svg"?></span>
+                        </div>
+                        News
+                    </a>
+                    <a href="#">
+                        <div class="icon1">
+                             <span><?php include"icons/photo-film.svg"?></span>
+                        </div>
+                        Take a tour
+
+                    </a>
+                    <a href="#">
+                        <div class="icon1">
+                             <span><?php include"icons/location-dot.svg"?></span>
+                        </div>
+                        Location and contacts
+                    </a>
+                </nav>
+            </div>
+        </div>
+        <div class="logo">
+            <img src="images/logo.png" alt="logo">
+        </div>
+    </div>
+    <div class="middle">
+        <div class="text1">
+            <img src="2021_01_12_23_29_IMG_8025.JPG" alt="">
+            <div class="opacity">
+                <div class="bg"></div>
+                <div>
+                    <h1>Decor for A Century</h1>
+                    <P>event of a life time</P>
+                </div>
+              
+            </div>
+        </div>
+        <div class="text">
+                <h2>
+                    Tell Your Fantastic Stories Without Using Words
+                    <br>Events and parties that are to be remembered
+                </h2>
+           <div>
+             <img src="images/three.jpg" alt="">
+             <img src="images/five.jpg" alt="">
+           </div>
+          
+           <div>
+            <img src="images/pic5.jpg" alt="">
+             <img src="images/pic4.jpg" alt="">
+           </div>
+          
+        </div>
+    </div>
+    <footer>
+        <div class="contact" style="height: 60px;">
+            <a href="contact.html">
+                Contact Us
+                <div class="icon">
+                    <span><?php include"icons/phone.svg"?></span>
+                </div>
+            </a>
+            <p> 0700966054 <br> 0754975422</p>
+            <a href="#">
+                <div class="icon">
+                 <span><?php include"icons/location-dot.svg"?></span>
+                </div>
+                 Location and contacts
+            </a>
+        </div>
+        <div class="follow">
+            <p>You can also follow us on our social media pages</p>
+            <div class="social">
+                <div>
+                    <div class="icon">
+                         <span><?php include"icons/facebook.svg"?></span>
+                    </div>
+                </div>
+                <div>
+                    <div class="icon">
+                        <span><?php include"icons/instagram.svg"?></span>
+                    </div>
+                </div>
+                <div>
+                    <div class="icon">
+                      <span><?php include"icons/whatsapp.svg"?></span>
+                    </div>
+                </div>
+                <div>
+                    <div class="icon">
+                        <span><?php include"icons/twitter.svg"?></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="quicklinks">
+            <p>Quick Links</p>
+            <div>
+                <a href="#">Location and contacts</a>
+                <a href="#">Events</a>
+                <a href="#">company profile</a>
+                <a href="#">Take a Tour</a>
+            </div>
+        </div>
+        <div class="copy">
+           <p> &copy;2022 Pryce concepts</p>
+        </div>
+    </footer>
+</body>
+</html>
